db47af76c8e55333a8a819235d3172e308a6763e
Raiss Bahati <raibahati@gmail.com>
2019-05-29 21:37:57 +0100

    First Guess Number Version

---
+++ Techdegree-Project-1/number_guessing_game.py
@@ -0,0 +1,36 @@
+"""
+Python Web Development Techdegree
+Project 1 - Number Guessing Game
+AUTHOR: RAISS BAHATI
+--------------------------------
+"""
+
+import random
+
+def start_game():
+
+    print("WELCOME TO THE NUMBER GUESSING GAME")
+    secret_number = random.randint(1, 20)
+    number_of_tries = 0
+
+    while True:
+        user_guess = int(input("Please enter a number between 1-20: "))
+        number_of_tries += 1
+
+        if user_guess < secret_number:
+            print("It's lower")
+
+        elif user_guess > secret_number:
+            print("It's higher")
+
+        else:
+            break
+
+    print("You got it!")
+    print("You guessed the number after {} tries!".format(number_of_tries))
+    print("Thank you for playing and see you soon")
+
+
+if __name__ == '__main__':
+    # Kick off the program by calling the start_game function.
+    start_game()
